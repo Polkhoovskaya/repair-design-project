77e52ac2d36c95af04c9a6485bcf54a2818f83fb
Olga <Polkhovskayaolga@mail.ru>
2019-10-07 00:55:24 +0300

    feat: add Sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 alert("HELLO FROM WEBPACK!");
 import { run } from "./app/app";
+import "./main.scss";
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader",//3
+              "css-loader",//2
+              "sass-loader"]//1
+      }
+    ]
   }
 };
